413f57980193fbeda085b4e77c77a8c5aba9303e
gmej <gmejias97@gmail.com>
2019-04-09 19:41:17 +0200

    LogIn & SignUp

--- app.js
+++ app.js
@@ -21,15 +21,64 @@ var SOCKET_LIST = {};
 //
 var DEBUG = true;
 
+
+let USERS = {
+    'a': 'a',
+    'b': 'b',
+    'c': 'c',
+}
+
+let isValidPassword = function (data) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(USERS[data.username] === data.password);
+        }, 1000);
+    });
+}
+
+let isUsernameTaken = function (data) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(USERS[data.username]);
+        }, 1000);
+    });
+}
+
+let addUser = function (data) {
+    console.log('data.username :', data.username);
+    console.log('data.password :', data.password);
+    USERS[data.username] = data.password;
+}
+
 var io = require('socket.io')(server);
 io.sockets.on('connection', socket => {
     socket.id = Math.random();
     SOCKET_LIST[socket.id] = socket;
 
-    Player.onConnect(socket);
-    
     socket.number = "" + Math.floor(10 * Math.random());
     console.log('client connected: ', socket.id);
+
+    socket.on('logIn', data => {
+        isValidPassword(data).then((result) => {
+            Player.onConnect(socket);
+            socket.emit('logInResponse', { success: true });
+            console.log('Client logged in');
+        }).catch((err) => {
+
+            socket.emit('logInResponse', { success: false });
+        });
+    });
+
+    socket.on('signUp', data => {
+        isUsernameTaken(data)
+            .then((result) => {
+                socket.emit('signUpResponse', { success: false });
+            }).catch((err) => {
+                addUser(data);
+                socket.emit('signUpResponse', { success: true });
+                console.log('Client signed up');
+            });
+    });
 
     socket.on('disconnect', () => {
         delete SOCKET_LIST[socket.id];
@@ -39,22 +88,23 @@ io.sockets.on('connection', socket => {
     socket.on('sendMsgToServer', data => {
         console.log(`[CHAT] ${data.id} says: ${data.msg}`);
         var playerName = "" + data.id;
-        for(let i in SOCKET_LIST) {
+        for (let i in SOCKET_LIST) {
             SOCKET_LIST[i].emit('addToChat', playerName + ': ' + data.msg);
         }
     });
 
     socket.on('evalServer', data => {
-        if(!DEBUG) 
+        if (!DEBUG)
             return;
-            try {
-                var res = eval(data);
-            } catch (error) {
-                console.log('[CHAT ERROR]');
-            }
+        try {
+            var res = eval(data);
+        } catch (error) {
+            console.log('[CHAT ERROR]: ');
+            console.log(error);
+        }
         socket.emit('evalAnswer', res);
     });
-    
+
 
 });
 
@@ -69,10 +119,10 @@ setInterval(() => {
     for (let i in SOCKET_LIST) {
         var socket = SOCKET_LIST[i];
         socket.emit('newPosition', packet);
-        
+
     }
 }, 1000 / 25);
 
 module.exports.data = {
-    
+
 }
---
+++ elXabas.js
@@ -0,0 +1,6 @@
+let factorial = function (number) {
+    if (number == 0 ){
+        return 1;
+    }
+    return number*factorial(number-1);
+}
